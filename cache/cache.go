ae385644b586f41d156b142a43148e43942f840d
Matthias Krull <m.krull@uninets.eu>
2015-08-29 16:25:10 +0200

    fix go routine leak; add doc

--- cache/cache.go
+++ cache/cache.go
@@ -10,12 +10,18 @@ var (
 	shards = 64
 )
 
+type entry struct {
+	value interface{}
+	exit  chan struct{}
+}
+
 type shard struct {
-	Entries map[string]interface{}
+	Entries map[string]entry
 	Stats   *Stats
 	sync.RWMutex
 }
 
+// Stats represents access statistics of a Cache.
 type Stats struct {
 	Hits    int       `json:"hits"`
 	Misses  int       `json:"misses"`
@@ -24,44 +30,73 @@ type Stats struct {
 	Uptime  time.Time `json:"uptime"`
 }
 
+// Cache is a thread safe structure to store and retrieve arbitrary values.
 type Cache []*shard
 
+// New returns a reference to a new Cache
 func New() *Cache {
 	c := make(Cache, shards)
 	for i := 0; i < shards; i++ {
-		c[i] = NewShard()
+		c[i] = newShard()
 	}
 	return &c
 }
 
-func NewShard() *shard {
+func newShard() *shard {
 	return &shard{
-		Entries: make(map[string]interface{}),
+		Entries: make(map[string]entry),
 		Stats:   &Stats{Uptime: time.Now().UTC()},
 	}
 }
 
+// Set stores the value with the given key.
 func (c *Cache) Set(key string, value interface{}) {
 	s := c.getShard(key)
 	s.Lock()
 	defer s.Unlock()
-	e := value
+
+	e, ok := s.Entries[key]
+	// make sure to exit the ttl go routine of a previously stored value
+	// before overwriting it
+	if ok && e.exit != nil {
+		e.exit <- struct{}{}
+	}
+
+	e.value = value
 	s.Entries[key] = e
+
 	s.Stats.Set++
 }
 
+// SetWithTTL stores the value with the given key and removes it automatically after
+// ttl seconds.
 func (c *Cache) SetWithTTL(key string, value interface{}, ttl int) {
 	s := c.getShard(key)
 	s.Lock()
 	defer s.Unlock()
-	e := value
-	timeout := time.Tick(time.Second * time.Duration(ttl))
+
+	e, ok := s.Entries[key]
+	// make sure to exit the ttl go routine of a previously stored value
+	// before overwriting it
+	if ok && e.exit != nil {
+		e.exit <- struct{}{}
+	}
+
+	e.value = value
+	e.exit = make(chan struct{}, 1)
+
 	s.Entries[key] = e
 	s.Stats.Set++
+
+	timeout := time.Tick(time.Second * time.Duration(ttl))
+	// wait for the timeout concurrently
 	go func() {
 		for {
 			select {
 			case <-timeout:
+				c.Remove(key)
+				return
+			case <-e.exit:
 				c.Remove(key)
 				return
 			}
@@ -75,6 +110,8 @@ func (c *Cache) getShard(key string) *shard {
 	return (*c)[uint(h.Sum32())%uint(shards)]
 }
 
+// Get retrieves a value stored with a specific key. If no value is available
+// nil and false will be returned.
 func (c *Cache) Get(key string) (interface{}, bool) {
 	s := c.getShard(key)
 	s.RLock()
@@ -84,26 +121,47 @@ func (c *Cache) Get(key string) (interface{}, bool) {
 
 	if ok {
 		s.Stats.Hits++
-	} else {
-		s.Stats.Misses++
+		return v.value, true
 	}
 
-	return v, ok
+	s.Stats.Misses++
+
+	return nil, ok
 }
 
+// Remove deletes a value stored with the given key from the cache.
+// In case no value exists no action is performed.
 func (c *Cache) Remove(key string) {
 	s := c.getShard(key)
 	s.Lock()
 	defer s.Unlock()
 
-	delete(s.Entries, key)
-	s.Stats.Removed++
+	e, ok := s.Entries[key]
+	if ok {
+		if e.exit != nil {
+			e.exit <- struct{}{}
+		}
+		delete(s.Entries, key)
+		s.Stats.Removed++
+	}
 }
 
+func (c *Cache) len() int {
+	return len(*c)
+}
+
+func (c *Cache) shard(n int) *shard {
+	return (*c)[n]
+}
+
+// GetStats returns Stats for this cache instance.
 func (c *Cache) GetStats() *Stats {
 	s := Stats{}
 
-	for i, shrd := range *c {
+	for i := 0; i < c.len(); i++ {
+		shrd := c.shard(i)
+		shrd.Lock()
+
 		if i == 0 {
 			s.Uptime = shrd.Stats.Uptime
 		}
@@ -111,6 +169,8 @@ func (c *Cache) GetStats() *Stats {
 		s.Misses += shrd.Stats.Misses
 		s.Set += shrd.Stats.Set
 		s.Removed += shrd.Stats.Removed
+
+		shrd.Unlock()
 	}
 
 	return &s
